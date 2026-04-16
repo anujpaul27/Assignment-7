2f7be85ea295700afee77e2ab220a522d26c9b74
anuj paul <paulanuj27@gmail.com>
2026-04-16 16:03:10 +0600

    Timeline page Added

---
+++ src/Component/Timeline.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+const Timeline = () => {
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+        const func = () => {
+            const savedData = localStorage.getItem('users');
+            if (savedData) {
+                setData(JSON.parse(savedData));
+            }
+        }
+        func();
+    }, [])
+
+
+    return (
+        <div className='w-10/12 mx-auto'>
+            <h1 className="p-4 pb-2 text-3xl opacity-60 tracking-wide">Timeline</h1>
+            {
+                data?.map(value =>
+                    <ul className="list bg-base-100 rounded-box shadow-md">
+
+                        <li className="list-row">
+                            <div>{value.image}</div>
+                            <div>
+                                <div>{value.str}</div>
+                                <div className="text-xs uppercase font-semibold opacity-60">{value.date}</div>
+                            </div>
+                            
+                        </li>
+
+                    </ul>
+                )
+            }
+        </div>
+    );
+};
+
+export default Timeline;
