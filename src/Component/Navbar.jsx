3ea48b90cee6d8c9115f34086f070470a5230f0b
anuj paul <paulanuj27@gmail.com>
2026-04-16 20:59:53 +0600

    Added Toggling Active button

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,17 +1,32 @@
-import { Link, Links } from "react-router";
+import { useState } from "react";
+import { Link, Links, NavLink } from "react-router";
 
 const Navbar = () => {
+    const [active, setActive] = useState('Home');
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">KinKeeper</a>
+                <NavLink to={'/'} className="btn btn-ghost text-xl">KinKeeper</NavLink>
             </div>
-            <div className="flex-none">
-                <ul className="menu flex gap-2  menu-horizontal px-1">
-                    <li><Link to={'/'} className='btn btn-outline lg:btn-md md:btn-sm btn-xs  '>Home</Link  ></li>
-                    <li><Link to={'/timeline'} className='btn btn-outline lg:btn-md md:btn-sm btn-xs  '>Timeline</Link  ></li>
-                    <li><Link to={'/starts'} className='btn btn-outline lg:btn-md md:btn-sm btn-xs  '>Stats</Link ></li>
-                </ul>
+            <div className="">
+                <Link to={'/'}
+                    className={`btn lg:btn-md btn-sm  ${active === 'Home' ? 'btn-active btn-primary' : ''}`}
+                    onClick={() => setActive('Home')}
+                >
+                    🏠 Home
+                </Link >
+                <Link to={'/timeline'}
+                    className={`btn lg:btn-md btn-sm  ${active === 'Timeline' ? 'btn-active btn-primary' : ''}`}
+                    onClick={() => setActive('Timeline')}
+                >
+                    📅 Timeline
+                </Link >
+                <Link to={'/starts'}
+                    className={`btn lg:btn-md btn-sm  ${active === 'Starts' ? 'btn-active btn-primary' : ''}`}
+                    onClick={() => setActive('Starts')}
+                >
+                    📈 Starts
+                </Link>
             </div>
         </div>
     );
--- src/Component/Starts.jsx
+++ src/Component/Starts.jsx
@@ -1,7 +1,36 @@
 import { Pie, PieChart, Tooltip } from 'recharts';
 import { RechartsDevtools } from '@recharts/devtools';
+import { useEffect, useState } from 'react';
 
 const Starts = () => {
+    const [call, setCall] = useState(0);
+    const [text, setText] = useState(0);
+    const [video, setVideo] = useState(0);
+
+    useEffect(() => {
+        // Get Data
+        const savedData = localStorage.getItem('users');
+        const historyData = savedData ? JSON.parse(savedData) : [];
+
+        let callCount = 0;
+        let textCount = 0;
+        let videoCount = 0;
+
+        // Count Type of call
+        for (let item of historyData) {
+            if (item.status === 'Call') callCount++;
+            if (item.status === 'Text') textCount++;
+            if (item.status === 'Video') videoCount++;
+        }
+
+        //  Update states 
+        setCall(callCount);
+        setText(textCount);
+        setVideo(videoCount);
+    }, []);
+
+
+
     return (
         <div className='w-10/12 mx-auto text-gray-400'>
             <h1 className='lg:text-3xl text-xl my-2   '>Friendship Analytics</h1>
@@ -10,9 +39,9 @@ const Starts = () => {
                 <Pie
 
                     data={[
-                        { name: 'Text', uv: 3, fill: '#0088FE' },
-                        { name: 'Call', uv: 5, fill: '#00C49F' },
-                        { name: 'Video', uv: 2, fill: '#FFBB28' },
+                        { name: 'Text', uv: text, fill: '#0088FE' },
+                        { name: 'Call', uv: call, fill: '#00C49F' },
+                        { name: 'Video', uv: video, fill: '#FFBB28' },
                     ]}
                     dataKey="uv"
                 />
@@ -25,7 +54,7 @@ const Starts = () => {
                 <p>  <span className='btn btn-circle btn-xs bg-[#00C49F]'></span> Call</p>
                 <p> <span className='btn btn-circle btn-xs bg-[#FFBB28]'></span> Video</p>
             </div>
-            
+
         </div>
     );
 };
